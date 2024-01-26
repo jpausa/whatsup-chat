07da56f13bd8eab90d1528757bf0ad6ef510f96b
Jorge <glph8911@gmail.com>
2024-01-26 09:03:48 -0500

    persist messages

--- server/index.js
+++ server/index.js
@@ -2,22 +2,59 @@ import express from 'express'
 import logger from 'morgan'
 import { createServer } from 'node:http'
 import { Server } from 'socket.io'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
+dotenv.config()
 const port = process.env.PORT || 3000
 
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
-io.on('connection', (socket) => {
+const db = createClient({
+  url: process.env.DB_URL,
+  authToken: process.env.DB_TOKEN,
+})
+
+await db.execute(
+  'CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)'
+)
+
+io.on('connection', async (socket) => {
   console.log('a user connected')
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:msg)',
+        args: { msg },
+      })
+    } catch (error) {
+      console.log(error)
+      return
+    }
+    io.emit('chat message', msg, result.lastInsertRowid.toString())
   })
+
+  if (!socket.recovered) {
+    try {
+      const result = await db.execute({
+        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0],
+      })
+      result.rows.forEach((element) => {
+        socket.emit('chat message', element.content, element.id.toString())
+      })
+    } catch (error) {
+      console.log(error)
+      return
+    }
+  }
 })
 
 app.use(logger('dev'))
